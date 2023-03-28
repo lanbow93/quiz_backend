5d736cf24cec187f21186476e48619bc7c826fa2
lanbow93 <the.doctor1501@gmail.com>
2023-03-28 14:37:54 -0500

    fixed localhost url

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ const app: Application = express();
 // Middleware
 app.use(morgan("tiny"));
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 app.use(express.json())
