b8ad3b7570cb31630da9eb5630655e1b6afc304b
lanbow93 <the.doctor1501@gmail.com>
2023-03-28 14:43:00 -0500

    open cors request

--- src/server.ts
+++ src/server.ts
@@ -19,10 +19,7 @@ const app: Application = express();
 
 // Middleware
 app.use(morgan("tiny"));
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
